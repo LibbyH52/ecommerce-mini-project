8ac4e216687521e4a19c33efdeb1d1b86b236d87
Libby Henry <libbyh52@gmail.com>
2019-05-27 18:31:34 +0000

    Forgot to add views.py in last 'git add'

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+def view_cart(request):
+    """A view that renders the cart contents"""
+    return render(request, "cart.html")
+    
+def add_to_cart(request, id):
+    """Add a qunatity of the specified product to the cart"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = cart.get(id) + quantity  
+    else:
+        cart[id] = cart.get(id, quantity)
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+    
+
+def adjust_cart(request):
+    """
+    Adjust the qunatity of a specified product
+    by a specified amount
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+    
+    
+    
+
