3bdf8fd333cf6f8b42c484c6c9649f1d798b4680
Libby Henry <libbyh52@gmail.com>
2019-05-27 11:18:10 +0000

    Created views, urls and templates to display products. Also, upload meda image and updated project urls

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 
 class EmailAuth:
-    """Authenticate a user by an exact matcu on the email and password"""
+    """Authenticate a user by an exact match on the email and password"""
     
     def authenticate(self, username=None, password=None):
         """
